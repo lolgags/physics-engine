898d9e0cffa12d903e9aed7803cc9edbeaa0ca17
bluesn0w <40858959+bsn0w@users.noreply.github.com>
2018-12-08 22:09:39 +0000

    Update particle.lua

--- objects/particle.lua
+++ objects/particle.lua
@@ -3,26 +3,31 @@ local vector3 = require("lib.vector3")
 local states = require("lib.states")
 local particle = class:derive("particle")
 
-function particle:new(x, v, a)
+function particle:new(x, v, a, m)
     self.position = x
     self.velocity = v
     self.acceleration = a
-
-    self.mass = 1
+    self.mass = m
+    
     self.force = vector3(0, 0, 0)
     self.gravity = 10
     
-    self.states = states("")
+    self.charge = 1.0 
 end
 
 function particle:update(time)
-    p1 = self.velocity:mul(time)
-    self.position = vector3(p1.x + self.position.x, p1.y + self.position.y, 0)
+    self.acceleration = self.force:mul(1 / self.mass)
 
     v1 = self.acceleration:mul(time)
-    self.velocity = vector3(v1.x + self.velocity.x, v1.y + self.velocity.y, 0)  
+    self.velocity = vector3(v1.x + self.velocity.x, v1.y + self.velocity.y, 0)
     
-    self.acceleration = self.force:mul(1 / self.mass)
+    p1 = self.velocity:mul(time)
+    self.position = vector3(p1.x + self.position.x, p1.y + self.position.y, 0)
+    --print(self.position)
+end
+
+function particle:draw()
+    love.graphics.circle("fill", self.position.x, self.position.y, self.mass * 3)
 end
 
 return particle
