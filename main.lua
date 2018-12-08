14f11ec638f0d92c2b62b21dbeefe3977d47c70b
bluesn0w <40858959+bsn0w@users.noreply.github.com>
2018-12-08 22:09:06 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,57 +1,33 @@
 local vector3 = require("lib.vector3")
-local camera = require("lib.camera")
 local timer = require("lib.timer")
 local particle = require("objects.particle")
+
 local forces = require("forces")
 
+gametime = timer()
+gametime:start()
+
 function love.load()
-    love.window.setMode(800, 800, {vsync = false}); love.window.setTitle("Integrum Physica")
-    width = 800
-    height = 800
+    love.graphics.setDefaultFilter("nearest", "nearest")
+    width, height = love.graphics.getDimensions()
 
-    testtimer = timer()
-
-    particles = {}
-    velocity = vector3(0, 0, 0)
-    acceleration = vector3(0, 0, 0)
-
-    p1 = particle(vector3(100, 200, 0), vector3(0.01, 0, 0), acceleration)    
-    p2 = particle(vector3(width / 2, height / 2, 0), velocity, acceleration)
-
-    table.insert(particles, p1)
-    table.insert(particles, p2)
+    testparticle = particle(vector3(width / 2, height / 2, 0), vector3(0, 0, 0), vector3(0, 0, 0), 20)
+    testparticle2 = particle(vector3(width / 4, height / 2, 0), vector3(0.010, 0.01, 0), vector3(0, 0, 0), 10)
 end
 
 function love.update()
-    testtimer:update()
-    time = testtimer:getTime()
+    gametime:update()
 
-    -- Update the particles seperately
+    testparticle:update(gametime:getTime())
+    testparticle2:update(gametime:getTime())
 
-    p1.force = forces:gravity(p1, p2)
-    p2.force = forces:gravity(p2, p1)
+    --testparticle.force = forces:gravity(testparticle, testparticle2)
+    testparticle2.force = forces:gravity(testparticle2, testparticle)
 
-    for p = 1, #particles do
-        particle = particles[p]
-        print(p, particle)
-        particle:update(time)
-    end
-
-    if love.keyboard.isDown("f") then
-        testtimer:start()
-    end
-
-    --print(p1.position:sub(p2.position):mag(), p2.position:sub(p1.position):mag())
+    print(gametime:getTime())
 end
 
 function love.draw()
-    local width = love.graphics.getWidth()
-    local height = love.graphics.getHeight()
-
-    love.graphics.setPointSize(5)
-    love.graphics.print("FPS: " .. love.timer.getFPS(), 10, 10)
-   
-    for index, particle in pairs(particles) do
-        love.graphics.points(particle.position.x, particle.position.y)
-    end    
+    testparticle:draw()
+    testparticle2:draw()
 end
