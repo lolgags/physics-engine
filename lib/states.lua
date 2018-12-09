4b9e1b00e0b693a33dd7e45d57d4bfc7aab35218
bluesn0w <40858959+bsn0w@users.noreply.github.com>
2018-12-09 00:59:09 +0000

    Update states.lua

--- lib/states.lua
+++ lib/states.lua
@@ -1,27 +1,11 @@
-local class = require("lib.class")
-local states = class:derive("states")
-
-function states:new(states)
-    self.states = states
-    self.state = nil
-end
-
-function states:set(state)
-    if self.states[state] then
-        self.state = state
-    end
-end
-
-function states:add(state)
-    if self.states[state] == nil then
-        table.insert(self.states, state)
-    end
-end
-
-function states:remove(state)
-    if self.states[state] then
-        table.remove(self.states, state)
-    end
-end
+states = {
+    "mass",
+    "charge",
+    "spin",
+    "parity",
+    "angular momentum",
+    "linear momentum",
+    "momentum"
+}
 
 return states
