988aa62a274aced20f0a0bfe7c1ac5ad82f75ad5
bluesn0w <40858959+bsn0w@users.noreply.github.com>
2018-12-09 00:57:55 +0000

    Update forces.lua

--- forces.lua
+++ forces.lua
@@ -3,23 +3,29 @@ local forces = class:derive("forces")
 local vector3 = require("lib.vector3")
 
 function forces:gravity(p1, p2)
-    unitvector = p2.position:sub(p1.position):unit()
-    r = p2.position:sub(p1.position):mag()
-    G = 0.0003
+    if p1.states.mass and p2.states.mass then
+        if p1.states.mass > 0 and p2.states.mass > 0 then
+            unitvector = p2.position:sub(p1.position):unit()
+            r = p2.position:sub(p1.position):mag()
+            G = 0.0003
 
-    gravity = unitvector:mul((G * p1.mass * p2.mass)/(r * r))
-    return gravity
+            gravity = unitvector:mul((G * p1.mass * p2.mass)/(r * r))
+            return gravity
+        end
+    end
 end
 
 function forces:electric(p1, p2)
-    q1 = p1.charge
-    q2 = p2.charge
-    unitvector = p2.position:sub(p1.position):unit()
-    r = p2.position:sub(p1.position):mag()
-    f = 0.001
-
-    Fe = unitvector:mul((-1 * f * q1 * q2)/(r^2))
-    return Fe
+    if p1.states.charge and p2.states.charge then
+        q1 = p1.states.charge
+        q2 = p2.states.charge
+        unitvector = p2.position:sub(p1.position):unit()
+        r = p2.position:sub(p1.position):mag()
+        f = 0.001
+    
+        Fe = unitvector:mul((-1 * f * q1 * q2)/(r^2))
+        return Fe    
+    end
 end
 
 return forces
