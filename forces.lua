ba1704561e1f97935535586cb0885d6143421850
bluesn0w <40858959+bsn0w@users.noreply.github.com>
2018-12-09 00:14:09 +0000

    Update forces.lua

--- forces.lua
+++ forces.lua
@@ -3,7 +3,6 @@ local forces = class:derive("forces")
 local vector3 = require("lib.vector3")
 
 function forces:gravity(p1, p2)
-    --print(p1.position.x, p2.position.x)
     unitvector = p2.position:sub(p1.position):unit()
     r = p2.position:sub(p1.position):mag()
     G = 0.0003
@@ -15,6 +14,12 @@ end
 function forces:electric(p1, p2)
     q1 = p1.charge
     q2 = p2.charge
+    unitvector = p2.position:sub(p1.position):unit()
+    r = p2.position:sub(p1.position):mag()
+    f = 0.001
+
+    Fe = unitvector:mul((-1 * f * q1 * q2)/(r^2))
+    return Fe
 end
 
 return forces
